667fb820558c3ed43cdd9280bf887edc10c8a6d4
i2adev <ialtuna04@gmail.com>
2025-10-20 16:17:45 +0200

    0.13.5 SE INTEGRAN LOS TIPOS DE LETRA EN EL ARCHIVO DE RECURSOS PARA QUE PUEDA FUNCIONAR EN CUALQUIER ORDENADOR. (SIN COMPILAR EL ARCHIVO DE RECURSOS)

--- src/main.py
+++ src/main.py
@@ -19,7 +19,7 @@ from Model.Entities.usuario_entity import UsuarioEntity
 from Views.Dialogs.login_dialog import LoginDialog
 
 # Versión del programa
-__version__ = "0.13.4"
+__version__ = "0.13.5"
 
 # Entrada al programa
 def main():
